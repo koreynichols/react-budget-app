1e8a7539c48a46e1aa5b86df7304408db9c24fba
Korey Nichols <koreynichols1@gmail.com>
2022-01-17 02:01:25 -0800

    BudgetCard completed

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         alignItems: "flex-start"
       }}
     >
-      <BudgetCard name="Entertainment" amount={800} max={1000}></BudgetCard>
+      <BudgetCard name="Entertainment" gray amount={1800} max={1000}></BudgetCard>
     </div>
   </Container>
 }
--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -1,9 +1,17 @@
 import { Card, ProgressBar, Stack, Button } from "react-bootstrap"
 import { currencyFormatter } from "../utils"
 
-export default function BudgetCard({ name, amount, max}) {
+export default function BudgetCard({ name, amount, max, gray }) {
+
+    const classNames = []
+    if (amount > max) {
+        classNames.push("bg-danger", "bg-opacity-10")
+    } else if (gray) {
+        classNames.push("bg-light")
+    }
+
     return (
-        <Card>
+        <Card className={classNames.join(" ")}>
             <Card.Body>
                 <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
                     <div className="me-2">{name}</div>
@@ -21,8 +29,8 @@ export default function BudgetCard({ name, amount, max}) {
                     now={amount}
                 />
                 <Stack direction="horizontal" gap="2" className="mt-4">
-                    <Button>Add Expense</Button>
-                    <Button>View Expenses</Button>
+                    <Button variant="outline-primary" className="ms-auto">Add Expense</Button>
+                    <Button variant="outline-secondary">View Expenses</Button>
                 </Stack>
             </Card.Body>
         </Card>
