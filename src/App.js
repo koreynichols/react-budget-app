e8642a3aca509816bba27a87f78a684556bf741a
Korey Nichols <koreynichols1@gmail.com>
2022-01-18 02:59:44 -0800

    created modal to add new budgets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import { Stack, Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
+import AddBudgetModal from "./components/AddBudgetModal";
 import BudgetCard from "./components/BudgetCard";
+import { useState } from "react";
 
 function App() {
-  return <Container className="my-4">
-    <Stack direction="horizontal" gap="2" className="mb-4">
-      <h1 className="me-auto">Budgets</h1>
-      <Button variant="primary">Add Budget</Button>
-      <Button variant="outline-primary">Add Expense</Button>
-    </Stack>
-    <div 
-      style={{ 
-        display:"grid", 
-        gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", 
-        gap: "1rem", 
-        alignItems: "flex-start"
-      }}
-    >
-      <BudgetCard name="Entertainment" gray amount={1800} max={1000}></BudgetCard>
-    </div>
-  </Container>
+
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
+
+  return (
+    <>
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
+          <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
+        <div 
+          style={{ 
+            display:"grid", 
+            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", 
+            gap: "1rem", 
+            alignItems: "flex-start"
+          }}
+        >
+          <BudgetCard name="Entertainment" gray amount={1800} max={1000}></BudgetCard>
+        </div>
+      </Container>
+      <AddBudgetModal show={showAddBudgetModal} handleClose={() => setShowAddBudgetModal(false)} />
+    </>
+  )
 }
 
 export default App;
